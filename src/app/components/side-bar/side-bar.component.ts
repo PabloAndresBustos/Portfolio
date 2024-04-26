22af1902034e6a26ceecddc6cae42fa6861e4f90
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-04-26 15:51:33 -0300

    Configuracion de cambio de menu

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren, inject, model } from '@angular/core';
+import { AfterViewInit, 
+         Component,
+         ElementRef,
+         HostListener,
+         OnInit,
+         QueryList,
+         ViewChildren,
+         inject,
+         model } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ViewsService } from 'app/services/views.service';
@@ -11,54 +19,55 @@ import { WidthExpandDirective } from '../directives/expand/width-expand.directiv
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent implements AfterViewInit, OnInit {
 
-  @ViewChild('welcome') welcome!: ElementRef;
+  viewsServices = inject(ViewsService)
+
   @ViewChildren('item') items!: QueryList<ElementRef>;
 
   /* Visibildada del SideBar */
   menu = model<boolean>(true);
+  width!:number;
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    const width = event.target.innerWidth;
-    console.log(width)
-    if (width >= 912) {
-      this.menu.set(true)
-    }
-
-    if (width <= 1364) {
-      this.menu.set(false)
-    }
+    this.windowSize()
   }
 
   @HostListener('click', ['$event'])
-  clickItem(event: any) {
-    if(event.target === this.welcome.nativeElement ||
-      this.items.toArray().some(item => event.target === item.nativeElement)
-    ){
-      this.menu.update(value => value = !value)
+  itemClick(event: any){
+    if(this.items.toArray()
+      .some(item => event.target === item.nativeElement) && this.width <= 912){
+      this.menu.update(value => value = false)
     }
   }
 
-  viewsServices = inject(ViewsService)
-
+  windowSize(){
+    this.width = window.innerWidth;
+    if (this.width >= 912) {
+      this.menu.update(value => value = true)
+    }else{
+      this.menu.update(value => value = false)
+    }
+  }
 
   hideShowMenu() {
     this.menu.update(value => value = !value);
   }
 
-  public showLanding = inject(ViewsService);
-
   hideShow() {
-    this.showLanding.hideShowLanding.update((value) => value = false);
+    this.viewsServices.hideShowLanding.update((value) => value = false);
   }
 
   showHome() {
-    this.showLanding.hideShowLanding.update((value) => value = true);
+    this.viewsServices.hideShowLanding.update((value) => value = true);
   }
 
   ngOnInit(): void {
     this.viewsServices.lenguaje()
+  }
+
+  ngAfterViewInit(): void {
+    this.windowSize();
   }
 }
