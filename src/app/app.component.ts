0c52cd366800fe977bb99817e340dbaffc3f1d52
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-04-28 15:51:11 -0300

    Correcion de Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, viewChild } from '@angular/core';
+import { Component, OnInit, inject, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { ContentComponent } from './components/content/content/content.component';
@@ -12,12 +12,20 @@ import { LanguajeComponent } from './components/languaje/languaje.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   public showLanding = inject(ViewsService);
 
   /* Barra lateral */
   sideBarShow:boolean = false;
   sideBarChange = viewChild.required(ContentComponent);
+
+  ngOnInit(): void {
+    window.addEventListener('popstate', ()=>{
+      this.showLanding.hideShowLanding.update(value =>  value = !value)
+      console.log('Se presiono el retroseso')
+    })
+  }
+
   
 }
